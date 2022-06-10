6cc12e00efafbc8b0e015e3028c67885de17f767
erica201193 <erica.mancini20@gmail.com>
2022-06-10 09:55:13 +0200

    Completed

--- js/app-vue.js
+++ js/app-vue.js
@@ -4,10 +4,17 @@ new Vue({
     data: {
         message: "Hello Vue!",
         classiTitolo: "text-success display-1",
-        immagine: "src=https://picsum.photos/200/300",
+        immagine: "https://picsum.photos/id/3/300/300",
 
     },
     methods: {
+
+        changeImage() {
+            const newImgId = Math.floor(Math.random() * 100) + 1;
+
+            this.immagine = `https://picsum.photos/id/${newImgId}/300/300`;
+        },
+        
         saluta: function() {
             console.log("hai cliccato sul pulsante saluta")
 
